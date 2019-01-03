76bdd057f25e76b0af0bbdad555c562fa9dbd1d8
feng <2995207228@qq.com>
2019-01-03 14:25:19 +0800

    尝试修复播放漏洞

--- src/spider/index.js
+++ src/spider/index.js
@@ -195,8 +195,9 @@ export async function getSearch ({keyword, page}) {
 }
 
 export async function getLyric (songMid) {
+  let source = cancelRequest('getLyric')
   let url = `https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg?songmid=${songMid}&g_tk=5381`
-  let lyric = JSON.parse((await baseRequest(url)).data.slice(18, -1))
+  let lyric = JSON.parse((await baseRequest(url, {cancelToken: source.token})).data.slice(18, -1))
   return new Lyric(lyric.lyric, lyric.trans)
 }
 
