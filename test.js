8eafe7ca6ee4c0607f668993db78eb9d1ac5a65b
caigoul <langisme@qq.com>
2019-04-02 11:00:06 +0800

    删除文件

--- test.js
+++
@@ -1,8 +0,0 @@
-import localStorage from 'electron-localstorage'
-
-const local = require('electron-localstorage')
-
-local.setItem('A', 'A')
-console.log(local.getItem('A'))
-local.setItem('A', 'B')
-console.log(localStorage.getItem('A'))
