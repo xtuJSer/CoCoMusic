e79be313f167d61b4e5af34c9a4dd83edd0c1f86
caigoul <langisme@qq.com>
2019-04-11 23:15:56 +0800

    F1~F12的键盘码映射

--- src/main/hotKey.js
+++ src/main/hotKey.js
@@ -166,6 +166,18 @@ export function getMap () {
     38: 'Up',
     39: 'Right',
     40: 'Down',
+    112: 'F1',
+    113: 'F2',
+    114: 'F3',
+    115: 'F4',
+    116: 'F5',
+    117: 'F6',
+    118: 'F7',
+    119: 'F8',
+    120: 'F9',
+    121: 'F10',
+    122: 'F11',
+    123: 'F12',
     get 91 () {
       if (process.platform !== 'darwin') {
         return 'Super'
