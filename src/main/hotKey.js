9bc36a5ad3df1761b4eb3f883b69e6cb40d6056d
caigoul <langisme@qq.com>
2019-04-11 13:38:13 +0800

    快捷键没设置完整的BUG

--- src/main/hotKey.js
+++ src/main/hotKey.js
@@ -126,19 +126,23 @@ export function setGlobalKey (type, keys) {
   if (keys.length < 1 || keys[0].length <= 1) {
     status = 'FAIL'
   } else {
-    globalShortcut.register(keys.join(' + '), () => {})
-    if (globalShortcut.isRegistered(keys.join(' + ')) === true) {
-      let tostoreSetting = {}
-      keyMap[type]['key'] = keys.join(' + ')
-      Object.keys(keyMap).forEach(k => {
-        tostoreSetting[k] = {
-          key: keyMap[k]['key']
-        }
-      })
-      config.default.setItem('keyMap', tostoreSetting)
-      status = 'SUCCESS'
-    } else {
-      status = '与 系统 / 其他程序 快捷键冲突'
+    try {
+      globalShortcut.register(keys.join(' + '), () => {})
+      if (globalShortcut.isRegistered(keys.join(' + ')) === true) {
+        let tostoreSetting = {}
+        keyMap[type]['key'] = keys.join(' + ')
+        Object.keys(keyMap).forEach(k => {
+          tostoreSetting[k] = {
+            key: keyMap[k]['key']
+          }
+        })
+        config.default.setItem('keyMap', tostoreSetting)
+        status = 'SUCCESS'
+      } else {
+        status = '与 系统 / 其他程序 快捷键冲突'
+      }
+    } catch (e) {
+      status = '快捷键没有设置完整'
     }
   }
   resetHotKey()
