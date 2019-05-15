7d897a2d0bd185e483a8b4188e90c78d06d823bd
fengT-T <2995207228@qq.com>
2019-05-15 14:49:53 +0800

    添加keepalive支持

--- src/spider/index.js
+++ src/spider/index.js
@@ -25,28 +25,17 @@
  * 后天又改回来，我们人多不怕，气不死也要累死你
  */
 import request from 'axios'
+import http from 'http'
+import https from 'https'
 import { Singer, Album, Music, Mv, Lyric, Category, PlayList } from './commonObject'
 
-const CancelToken = request.CancelToken
+const keepAliveConf = {
+  keepAlive: true
+}
 
-const cancelMap = {
-  getSingerMusicList: null,
-  getSingerAlbumList: null,
-  getSingerMvList: null,
-  getAlbum: null,
-  getPlayListInfo: null,
-  getPlayList: null
-}
-/**
- * c++ 天下第一，客户端，嵌入式，服务器都能做，老子这辈子就学他了。
- * JavaScript 真好用。
- * @param {String} key
- */
-function cancelRequest (key) {
-  cancelMap[key] && cancelMap[key].cancel(`cancel`)
-  cancelMap[key] = null
-  return (cancelMap[key] = CancelToken.source())
-}
+const httpAgent = new http.Agent(keepAliveConf)
+const httpsAgent = new https.Agent(keepAliveConf)
+
 /**
  * fuck webpack, Ruined my day
  * 真的是日乐购了,我特么草拟吗.
@@ -57,7 +46,9 @@ const baseRequest = request.create({
   headers: {
     'Referer': 'http://y.qq.com/portal/player.html',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
-  }
+  },
+  httpAgent,
+  httpsAgent
   // timeout: 2000 // 这个时间不好把握，我只能说小霸王服务器该换了
 })
 
@@ -68,7 +59,8 @@ export async function getSingerList ({ page, country, name }) {
   let {data: {list, total_page}} = (await baseRequest(url)).data 
   return {
     totalPage: total_page, 
-    singerList: list.map(({Fsinger_name, Fsinger_mid}) => new Singer(Fsinger_name, Fsinger_mid))
+    singerList: list.map(
+      ({Fsinger_name, Fsinger_mid}) => new Singer(Fsinger_name, Fsinger_mid))
   }
   /* eslint-disable */
 }
@@ -85,12 +77,17 @@ export async function getNewSingerList ({page, area, sex, genre, index}) {
 }
 // page 从 0 开始
 export async function getSingerMusicList ({page, singerMid}) {
-  let source = cancelRequest('getSingerMusicList')
   let url = `http://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg?&singermid=${singerMid}&order=listen&begin=${page*30}&num=30`
-  let {data: {list, total}} = (await baseRequest(url, {cancelToken: source.token})).data
+  let {data: {list, total}} = (await baseRequest(url)).data
   return {
     total: Math.floor(total / 30),
-    list: list.map(({musicData: {songmid, strMediaMid, songname, albumname, albummid, singer, type}}) => new Music(songname, songmid, strMediaMid, new Album(albumname, albummid), singer.map(({mid, name}) => new Singer(name,mid)), type))
+    list: list.map(
+      ({musicData: {songmid, strMediaMid, songname, albumname, albummid, singer, type}}) =>
+        new Music(songname, songmid, strMediaMid,
+          new Album(albumname, albummid),
+          singer.map(
+            ({mid, name}) => new Singer(name,mid))
+          , type))
   }
 }
 
@@ -131,19 +128,17 @@ export async function getSingerInfo ({singerMid}) {
   })
 }
 export async function getSingerAlbumList ({singerMid, page}) {
-  let source = cancelRequest('getSingerAlbumList')
   let url = `http://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_album.fcg?singermid=${singerMid}&order=time&begin=${page * 30}&num=30`
-  let {list, total} = (await baseRequest(url, {cancelToken: source.token})).data.data
+  let {list, total} = (await baseRequest(url)).data.data
   return {
     total: Math.floor(total / 30),
     list: list ? list.map(({albumMID, albumName}) => new Album(albumName, albumMID)) : []
   }
 }
 export async function getSingerMvList ({singerMid, page}) {
-  let source = cancelRequest('getSingerMvList')
   // cid  是啥？ 我也不知道，我也很绝望啊，https://y.gtimg.cn/music/portal/js/v4/singer_afadc5b.js?max_age=31536000 代码里面直接写死了的，我好慌
   let url =  `http://c.y.qq.com/mv/fcgi-bin/fcg_singer_mv.fcg?singermid=${singerMid}&order=listen&begin=${page * 35}&num=35&cid=205360581`
-  let {list, total} = (await baseRequest(url, {cancelToken: source.token})).data.data
+  let {list, total} = (await baseRequest(url)).data.data
   return {
     total: Math.floor(total / 35),
     list: list ? list.map(({title, pic, vid}) => new Mv(title, pic, vid)) : []
@@ -172,9 +167,8 @@ export async function getKey (guid) {
 }
 export async function getSongVkey({fileName, guid, songMid}) {
   // cid 是啥？ 我也不造啊 qq 那群人写死了
-  let source = cancelRequest('getSongVkey')
   let url =  `http://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg?cid=205361747&songmid=${songMid}&filename=${fileName}&guid=${guid}`
-  let {vkey} = (await baseRequest(url, {cancelToken: source.token})).data.data.items[0]
+  let {vkey} = (await baseRequest(url)).data.data.items[0]
   return {vkey}
 }
 // 从 1 开始
@@ -191,23 +185,34 @@ export async function getSearch ({keyword, page}) {
       break;
   }
   return {direct, totalPage: Math.ceil(totalnum / 20),
-    songList: list.map(({name, mid, file: {media_mid}, singer, album, type}) => new Music(name, mid, media_mid, new Album(album.name, album.mid), singer.map(singerItem => new Singer(singerItem.name, singerItem.mid)), type))
+    songList: list.map(
+      ({name, mid, file: {media_mid}, singer, album, type}) =>
+        new Music(name, mid, media_mid,
+          new Album(album.name, album.mid),
+          singer.map(
+            singerItem =>
+              new Singer(singerItem.name, singerItem.mid)),
+          type))
   }
 }
 
 export async function getLyric (songMid) {
-  let source = cancelRequest('getLyric')
   let url = `http://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg?songmid=${songMid}&g_tk=5381`
-  let lyric = JSON.parse((await baseRequest(url, {cancelToken: source.token})).data.slice(18, -1))
+  let lyric = JSON.parse((await baseRequest(url)).data.slice(18, -1))
   return new Lyric(lyric.lyric, lyric.trans)
 }
 
 export async function getAlbum ({albumMid}) {
-  let source = cancelRequest('getAlbum')
   let url = `http://c.y.qq.com/v8/fcg-bin/fcg_v8_album_info_cp.fcg?albummid=${albumMid}&g_tk=5381&hostUin=0&notice=0&platform=yqq&needNewCode=0`
-  let {list, name} = (await baseRequest(url, {cancelToken: source.token})).data.data
+  let {list, name} = (await baseRequest(url)).data.data
   return {
-    musicList: list.map(({songname, songmid, strMediaMid, albumname, albummid, singer, type}) => new Music(songname, songmid, strMediaMid, new Album(albumname, albummid), singer.map(({mid, name}) => new Singer(name, mid)), type)),
+    musicList: list.map(
+      ({songname, songmid, strMediaMid, albumname, albummid, singer, type}) =>
+        new Music(songname, songmid, strMediaMid,
+          new Album(albumname, albummid),
+          singer.map(
+            ({mid, name}) => new Singer(name, mid)),
+          type)),
     album: new Album(name, albumMid)
   }
 }
@@ -215,25 +220,36 @@ export async function getAlbum ({albumMid}) {
 export async function getCategory () {
   let url = 'http://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_tag_conf.fcg?g_tk=5381&notice=0&inCharset=utf8&outCharset=utf-8&platform=yqq&needNewCode=0'
   let {categories} = JSON.parse((await baseRequest(url)).data.slice(18, -1)).data
-  return categories.map(({categoryGroupName, items}) => { return {categoryGroupName, categoryList: items.map(({categoryId, categoryName}) => new Category(categoryName, categoryId))} })
+  return categories.map(
+    ({categoryGroupName, items}) => ({
+      categoryGroupName,
+      categoryList: items.map(
+        ({categoryId, categoryName}) => new Category(categoryName, categoryId))
+      }))
 }
 
 export async function getPlayList ({categoryId, page}) {
-  let source = cancelRequest('getPlayList')
   let url = `http://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg?picmid=1&rnd=${Math.random()}&g_tk=5381&jsonpCallback=getPlaylist&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&categoryId=${categoryId}&sortId=5&sin=${(page - 1) * 30}&ein=${page * 30 - 1}`
-  let {list, sum} = JSON.parse((await baseRequest(url, {cancelToken: source.token})).data.slice(12, -1)).data
+  let {list, sum} = JSON.parse((await baseRequest(url)).data.slice(12, -1)).data
   return {
-    list: list.map(({dissname, imgurl, dissid}) => new PlayList(dissid, dissname, imgurl)),
+    list: list.map(
+      ({dissname, imgurl, dissid}) =>
+        new PlayList(dissid, dissname, imgurl)),
     totalPage: Math.ceil(sum / 40)
   }
 }
 
 export async function getPlayListInfo (playListMid) {
-  let source = cancelRequest('getPlayListInfo')
   let url = `http://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?type=1&json=1&utf8=1&onlysong=0&disstid=${playListMid}&g_tk=5381&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0`
-  let {cdlist: [{dissname, songlist}]} = JSON.parse((await baseRequest(url, {cancelToken: source.token})).data.slice(13, -1))
+  let {cdlist: [{dissname, songlist}]} = JSON.parse((await baseRequest(url)).data.slice(13, -1))
   return {
     playListName: dissname,
-    list: songlist.map(({songname, songmid, strMediaMid, albumname, albummid, singer, type}) => new Music(songname, songmid, strMediaMid, new Album(albumname, albummid), singer.map(({mid, name}) => new Singer(name, mid)), type))
+    list: songlist.map(
+      ({songname, songmid, strMediaMid, albumname, albummid, singer, type}) =>
+        new Music(songname, songmid, strMediaMid,
+          new Album(albumname, albummid),
+          singer.map(
+            ({mid, name}) => new Singer(name, mid))
+          , type))
   }
 }
