89c00d407a9854210c38e05cdd143985dcca0f5a
fengT-T <2995207228@qq.com>
2020-03-11 00:56:47 +0800

    :beers: 搜索 api

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,23 +1,18 @@
 import { baseRequest } from './base-request'
 
 interface SongSearchList {
-  curnum: number;
-  curpage: number;
-  totalnum: number;
-  list: [{
-    chinesesinger: number;
-    language: number;
-    mid: string;
-    name: string;
-    status: number;
-    subtitle: string;
-    t: number;
-    tag: number;
-    time_public: string;
-    title: string;
-    title_hilight: string;
-    type: number;
-  }];
+  chinesesinger: number;
+  language: number;
+  mid: string;
+  name: string;
+  status: number;
+  subtitle: string;
+  t: number;
+  tag: number;
+  time_public: string;
+  title: string;
+  title_hilight: string;
+  type: number;
 }
 
 interface SearchDirection {
@@ -31,21 +26,129 @@ interface SearchDirection {
     songNum: number;
   };
 }
+interface AlbumSearchList {
+  albumMID: string;
+  albumName: string;
+  albumPic: string;
+  publicTime: string;
+  singerMID: string;
+  singerName: string;
+  singer_list: [{
+    mid: string;
+    name: string;
+  }];
+  song_count: number;
+  type: number;
+}
 
-interface SearchList {
-  song?: SongSearchList;
+interface MvSearchList {
+  mv_name: string;
+  mv_pic_url: string;
+  pay: number;
+  play_count: number;
+  publish_date: string;
+  singerMID: string;
+  singer_list: [{
+    mid: string;
+    name: string;
+  }];
+  singer_name: string;
+  type: number;
+  v_id: string;
+  video_switch: number;
+}
+
+interface PlayListSearchList {
+  num_per_page: number;
+  page_no: number;
+  display_num: number;
+  list: [{
+    copyrightnum: number;
+    createtime: string;
+    creator: Creator;
+    diss_status: number;
+    dissid: string;
+    dissname: string;
+    docid: number;
+    imgurl: string;
+    introduction: string;
+    listennum: number;
+    score: number;
+    song_count: number;
+  }];
+}
+interface Creator {
+  avatarUrl: string;
+  creator_uin: string;
+  encrypt_uin: string;
+  followflag: number;
+  isVip: number;
+  name: string;
+  qq: number;
+  singerid: number;
+  singermid: string;
+  type: number;
+}
+
+interface SearchList<T> {
+  curnum: number;
+  curpage: number;
+  totalnum: number;
+  list: T;
+}
+
+interface SearchMap {
+  song: SearchList<SongSearchList>;
+  album: SearchList<AlbumSearchList>;
+  mv: SearchList<MvSearchList>;
+}
+
+type SearchData = {
+  [T in keyof SearchMap]: SearchMap[T];
+} & {
   zhida: SearchDirection;
 }
 
-export async function searchMusic (keyword = '', num = 10, page = 1) {
-  const url = 'https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.center&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=10&w=%E5%88%9D%E9%9F%B3%E6%9C%AA%E6%9D%A5&g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0'
-  const params = {
-    w: keyword,
-    n: num,
-    p: page
+const searchParams = {
+  song: {},
+  album: {
+    sem: 10,
+    t: 8
+  },
+  mv: {
+    sem: 1,
+    t: 12
   }
-  const data = (await baseRequest({
-    url, params
-  })).data
-  return data.data as SearchList
+}
+
+/**
+ * 搜索
+ * @param type 搜索类型 "song" | "album" | "mv"
+ * @param keyword 搜索关键字
+ * @param num 搜索列表数量 10
+ * @param page 搜索列表页码 1
+ */
+export async function search (types: keyof SearchMap, keyword = '', page = 1, num = 10) {
+  const url = 'http://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&catZhida=1&g_tk=5381&format=json'
+  const params = {
+    w: decodeURIComponent(keyword), n: num, p: page, ...searchParams[types]
+  }
+  const data = (await baseRequest({ url, params })).data
+
+  return data.data as SearchData
+}
+/**
+ * 搜索歌单
+ * @param keyword 关键字
+ * @param page 搜索列表页码 0
+ * @param num 搜索列表数量 5
+ */
+export async function searchPlayList (keyword = '', page = 0, num = 5) {
+  const url = 'https://c.y.qq.com/soso/fcgi-bin/client_music_search_songlist?remoteplace=txt.yqq.playlist&flag_qc=0&g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0'
+  const params = {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    query: keyword, page_no: page, num_per_page: num
+  }
+  const data = (await baseRequest({ url, params })).data
+  return data.data as PlayListSearchList
 }
--- tests/unit/api/search.spec.ts
+++ tests/unit/api/search.spec.ts
@@ -1,9 +1,28 @@
 import { expect } from 'chai'
-import { searchMusic } from '../../../src/api/search'
+import { search, searchPlayList } from '../../../src/api/search'
 
 describe('搜索歌曲', function () {
   it('searchMusic', async function () {
-    const data = await searchMusic('初音未来')
+    const data = await search('song', '初音未来')
+    expect(data.song.list).to.have.length.above(1)
     expect((data.zhida.zhida_singer || {}).singerName).to.be.equal('初音未来 (初音ミク)')
+  })
+})
+describe('搜索Mv', function () {
+  it('searchMv', async function () {
+    const data = await search('mv', '初音未来')
+    expect(data.mv.list).to.have.length.above(1)
+  })
+})
+describe('搜索专辑', function () {
+  it('searchAlbum', async function () {
+    const data = await search('album', '初音未来')
+    expect(data.album.list).to.have.length.above(1)
+  })
+})
+describe('搜索歌单', function () {
+  it('searchPlayList', async function () {
+    const data = await searchPlayList('初音未来')
+    expect(data.list).to.have.length.above(1)
   })
 })
